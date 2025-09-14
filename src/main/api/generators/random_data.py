999e2a95be49757565704180218cb6a4ad7fa759
GLskill <Glebluz1488@gmail.com>
2025-09-14 23:27:27 +0200

    The second level of testing has been completed.

---
+++ src/main/api/generators/random_data.py
@@ -0,0 +1,19 @@
+import random
+
+from faker import Faker
+
+faker = Faker()
+
+
+class RandomData:
+    @staticmethod
+    def get_username() -> str:
+        return ''.join(faker.random_letters(length=random.randint(3, 15)))
+
+    @staticmethod
+    def get_password() -> str:
+        upper = [letter.upper() for letter in faker.random_letters(length=3)]
+        lower = [letter.lower() for letter in faker.random_letters(length=3)]
+        digits = [str(faker.random_digit()) for _ in range(3)]
+        special = [random.choice('!@#$%^&*')]
+        return random.shuffle(upper + lower + digits + special)
---
+++ src/main/api/requests/admin_user_requester.py
@@ -0,0 +1,21 @@
+import requests
+from  http import HTTPStatus
+from src.main.api.models.create_user_request import CreateUserRequest
+from src.main.api.models.create_user_response import CreateUserResponse
+from src.main.api.requests.requester import Requester
+
+
+class AdminUserRequester(Requester):
+    def post(self, create_user_request: CreateUserRequest) -> CreateUserResponse:
+        url = f'{self.base_url}/admin/users'
+        response = requests.post(url=url, json=create_user_request.model_dump(), headers=self.headers)
+        self.request_spec(response)
+        if response.status_code in [HTTPStatus.OK, HTTPStatus.CREATED]:
+            return CreateUserResponse(**response.json())
+
+    def delete(self, id: int):
+        url = f'{self.base_url}/admin/users/{id}'
+        response = requests.delete(url=url, headers=self.headers)
+        self.request_spec(response)
+        return response
+
