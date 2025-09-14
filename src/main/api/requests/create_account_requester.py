b3430f9945c6e45719bf9f216639b2450cb1483a
GLskill <Glebluz1488@gmail.com>
2025-09-14 23:27:27 +0200

    The second level of testing has been completed.
    .
    
    fixed the code
    
    added test on iteration 2 (transfer, deposit)
    
    created Transfer and Deposit tests
    
    fixed tests
    
    fixed tests
    
    fixed tests - 8 OK
    
    fixed tests - 8 OK

---
+++ src/main/api/requests/create_account_requester.py
@@ -0,0 +1,20 @@
+import requests
+
+from src.main.api.models.create_account_response import CreateAccountResponse
+from src.main.api.requests.requester import Requester
+
+
+class CreateAccountRequester(Requester):
+    def post(self) -> CreateAccountResponse:
+        url = f'{self.base_url}/accounts'
+        response = requests.post(url=url, headers=self.headers)
+        self.response_spec(response)
+        return CreateAccountResponse(**response.json())
+
+    def get(self) -> CreateAccountResponse:
+        url = f'{self.base_url}/customer/accounts'
+        response = requests.get(url=url, headers=self.headers)
+        self.response_spec(response)
+        return CreateAccountResponse(**response.json())
+
+
