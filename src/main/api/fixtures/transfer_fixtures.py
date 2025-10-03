cbba33cc63f31471afd4986063a5e404036b836b
GLskill <Glebluz1488@gmail.com>
2025-10-03 19:13:48 +0200

    Make Senior level TRANSFER and DEPOSIT test

---
+++ src/main/api/fixtures/transfer_fixtures.py
@@ -0,0 +1,26 @@
+import pytest
+
+from src.main.api.classes.api_manager import ApiManager
+from src.main.api.generators.random_data import RandomData
+from src.main.api.models.create_user_request import CreateUserRequest
+from src.main.api.models.transfer_request import TransferRequest
+
+
+@pytest.fixture
+def transfer_setup(api_manager: ApiManager, deposit_setup):
+    api_manager.user_steps.deposit_to_account(
+        deposit_setup["user"],
+        deposit_setup["deposit_request"]
+    )
+    user2 = CreateUserRequest(username=RandomData.get_username(), password=RandomData.get_password(), role="USER")
+    api_manager.admin_steps.create_user(user2)
+    account2 = api_manager.user_steps.create_account(user2)
+
+    transfer_request = TransferRequest(
+        senderAccountId=deposit_setup["deposit_request"].id,
+        receiverAccountId=account2.id,
+        amount=deposit_setup["deposit_amount"] * 0.5
+    )
+
+    yield {"transfer_request": transfer_request, "user1": deposit_setup["user"]}
+
--- src/main/api/specs/request_specs.py
+++ src/main/api/specs/request_specs.py
@@ -6,7 +6,6 @@ from src.main.api.models.login_user_request import LoginUserRequest
 from src.main.api.requests.skeleton.endpoint import Endpoint
 from src.main.api.requests.skeleton.requesters.crud_requester import CrudRequester
 from src.main.api.specs.response_specs import ResponseSpecs
-from src.main.api.configs.config import Config
 
 
 class RequestSpecs:
